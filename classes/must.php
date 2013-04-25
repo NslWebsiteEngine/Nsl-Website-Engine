6c50d79530f1235f83aacb8f1e36258ce46d50e5
Danny Morabito <staff@newsocialife.com>
2013-04-25 19:19:16 +0300

    Ops, copied wrong file :P

--- classes/must.php
+++ classes/must.php
@@ -1,59 +1,30 @@
 <?php
-class lib {
-  protected $__keywords__;
-	protected $__keywords_counter__ = 0xF0;
-	function __construct() {
-		$this->keyword("more");
-		$this->keyword("ok");
-		$this->keyword("already_there");
+class must {
+	function get($params, $callback) {
+		$this->handle($_GET, $params, $callback);
 	}
-	function __get($name) {
-		if(!isset($this->$name))
-			die("NSL ERROR: Class {$name} isn't loaded");
+	function post($params, $callback) {
+		$this->handle($_POST, $params, $callback);
 	}
-	function add($protocol) {
-		$protocol = strtolower($protocol);
-		if(!isset($this->$protocol)) {
-			$name = "classes/".strtolower($protocol).".php";
-			if(file_exists($name))
-				include $name;
-			else
-				die("NSL ERROR: Unable to find {$protocol} class.");
-			$this->$protocol = new $protocol;
-			if(isset($this->$protocol->__requirements)) {
-				foreach($this->$protocol->__requirements as $k => $v)
-					$this->add($v);
-				return $this->keyword("more");
+	function cookie($params, $callback) {
+		$this->handle($_COOKIE, $params, $callback);
+	}
+	function handle($arr, $params, $callback = null, $callbackelse = null) {
+		if(is_null($callback))
+			$callback = function($name) {
+				die("NSL ERROR: Parameter {$name} is required but isn't set");
+			};
+		if(is_null($callbackelse))
+			$callbackelse = function() {
+				return true;
+			};
+		if(is_array($params)) {
+			foreach($params as $param) {
+				if(!isset($arr[$param]))
+					$callback($param);
 			}
-			return $this->keyword("ok");
-		}
-		return $this->keyword("already_there");
-	}
-	function set($protocol) { return $this->add($protocol); }
-	function using($protocol) { return $this->add($protocol); }
-	function protocol($protocol) {
-		$protocol = strtolower($protocol);
-		return isset($this->$protocol) ? $this->$protocol : null;
-	}
-	function keyword($k) {
-		if(!isset($this->__keywords__[$k])) {
-			$this->__keywords__[$k] = $this->__keywords_counter__;
-			$this->__keywords_counter__ += strlen($k);
-		}
-		return dechex($this->__keywords__[$k]);
-	}
-	static function toObject($array) {
-		$obj = new stdClass();
-		foreach ($array as $key => $val)
-			$obj->$key = is_array($val) ? self::toObject($val) : $val;
-		return $obj;
-	}
-	static function toArray($d) {
-		if (is_object($d))
-			$d = get_object_vars($d);
-		if (is_array($d))
-			return array_map(__FUNCTION__, $d);
-		else
-			return $d;
+		}else
+			if(!isset($arr[$params]))
+				$callback($params);
 	}
 }
