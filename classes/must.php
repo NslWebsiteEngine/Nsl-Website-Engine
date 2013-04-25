c97c3c8b4143c735af706bedec061d58cd9f9e3a
Danny Morabito <staff@newsocialife.com>
2013-04-25 19:15:44 +0300

    Add must class
    The must class manages the handle requirements. For example a POST paramter is required for running a page.

---
+++ classes/must.php
@@ -0,0 +1,30 @@
+<?php
+class must {
+  function get($params, $callback) {
+		$this->handle($_GET, $params, $callback);
+	}
+	function post($params, $callback) {
+		$this->handle($_POST, $params, $callback);
+	}
+	function cookie($params, $callback) {
+		$this->handle($_COOKIE, $params, $callback);
+	}
+	function handle($arr, $params, $callback = null, $callbackelse = null) {
+		if(is_null($callback))
+			$callback = function($name) {
+				die("NSL ERROR: Parameter {$name} is required but isn't set");
+			};
+		if(is_null($callbackelse))
+			$callbackelse = function() {
+				return true;
+			};
+		if(is_array($params)) {
+			foreach($params as $param) {
+				if(!isset($arr[$param]))
+					$callback($param);
+			}
+		}else
+			if(!isset($arr[$params]))
+				$callback($params);
+	}
+}
