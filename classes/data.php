b08e900024e1c2567348b85e5b2afd648448ef82
Danny Morabito <staff@newsocialife.com>
2013-05-08 19:19:12 +0200

    Data class rewritten

--- classes/data.php
+++ classes/data.php
@@ -1,56 +1,22 @@
 <?php
 class data extends base {
-	function nicetime($fromDate, $toDate = NULL, $precision = -1, $separator = ', ', $divisors = NULL) {
-		$dates = [$fromDate, $toDate];
-		$difference = time() - $fromDate;
-		return $this->format_interval($difference, $precision, $separator, $divisors);
-	}
-	function format_interval($seconds, $precision = -1, $separator = ', ', $divisors = NULL) {
-		if(!isset($divisors)) {
-			$divisors = [
-				'Year' => [
-					"time" => 31536000,
-					"multiple" => "Years"
-				], 
-				'Month' => [
-                    "time" => 2628000,
-                    "multiple" => "Months"
-                ], 
-                "Day" => [
-                    "time" => 86400,
-                    "multiple" => "Days"
-                ],
-                "Hour" => [
-                    "time" => 3600,
-                    "multiple" => "Hours"
-                ],
-                "Minute" => [
-                    "time" => 60,
-                    "multiple" => "Minutes"
-                ],
-                "Second" => [
-                    "time" => 1,
-                    "multiple" => "Seconds"
-                ]
-			];
-		}
-		arsort($divisors);
-		foreach($divisors as $name => $divisor) {
-			if($value = floor($seconds / $divisor["time"])) {
-                if(!in_array($name, ["Ora", "Minuto", "Secondo"]))
-                    return "{$value} ".($value == 1 ? $name : $divisor["multiple"]);
-				if($value == 1)
-					$out[] = "$value $name";
-				else
-					$out[] = "$value ".$divisor["multiple"];
-				if(--$precision == 0)
-					break;
-			}
-			$seconds %= $divisor["time"];
-		}
-		if(!isset($out))
-			$out[] = "0 ".$name;
-		return implode($separator, $out);
-
-	}
+    function __get($time) {
+        $time = substr($time, 1);
+        $time = time() - $time;
+        $divisors = ['year' => 31536000, 'month' => 2628000, 'day' => 86400, 'hour' => 3600, 'minute' => 60, 'second' => 1];
+        $out = [];
+        foreach($divisors as $name => $divisor) {
+            if($value = floor($time / $divisor)) {
+                if(!in_array($name, ["hour", "minute", "second"]))
+                    $out[] = "{$value} {$name}".($value == 1 ? "" : "s");
+                else
+                    $out[] = "{$value} {$name}".($value == 1 ? "" : "s");
+            }
+            $time %= $divisor;
+        }
+        if(empty($out))
+            $out[] = "just now";
+        return implode(", ", $out);
+        
+    }
 }
