76195d43ec31cbd9d0f81e38ac0db529d0dd3e1b
Danny Morabito <staff@newsocialife.com>
2013-05-08 17:03:35 +0200

    Add json_encode_file() and is_json() for simplifing the json management. Add usenslargs() function to set-up url args separated with ; instend of &

--- classes/utils.php
+++ classes/utils.php
@@ -1,10 +1,24 @@
 <?php
 class utils {
+    function is_json($json) {
+        if(substr($json, 0, 1) != "{" && substr($json, 0, 1) != "[" && substr($json, -1) != "}" && substr($json, -1) != "]")
+            return false;
+        $json = json_decode($json);
+        return json_last_error() == JSON_ERROR_NONE;
+    }
     function json_decode_file($file) {
     	return json_decode(file_get_contents("{$file}"));
 	}
-    function toObject($array) {
-    	$obj = new stdClass();
+    function usenslargs() {
+        ini_set('arg_separator.output',';');
+    }
+    function json_encode_file($file, $contents) {
+        if(!$this->is_json($contents))
+            return file_put_contents($file, json_encode($contents));
+        return file_put_contents($file, $contents);
+    }
+	function toObject($array) {
+		$obj = new stdClass();
 		foreach ($array as $key => $val)
 			$obj->$key = is_array($val) ? self::toObject($val) : $val;
 		return $obj;
