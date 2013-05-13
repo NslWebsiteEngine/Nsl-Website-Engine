87c0f98ee743dfa9cc94c2b3e181ffb910b68613
Danny Morabito <staff@newsocialife.com>
2013-05-13 17:05:23 +0200

    Add time addX functions to data class

--- classes/data.php
+++ classes/data.php
@@ -16,7 +16,40 @@ class data extends base {
 		}
 		if(empty($out))
 			$out[] = "just now";
-		return implode(", ", $out);
-		
+		return implode(", ", $out);	
 	}
+    function sum($btime, $time, $format = "yyyy-mm-dd") {
+        $format = str_replace(["/", "."], "-", $format);
+        if(is_int($time))
+            return $btime + $time;
+        elseif(is_string($time)) {
+            if($format == "yyyy-mm-dd")
+                $time = strtotime($time);
+            elseif($format == "dd-mm-yyyy") {
+                $_time = explode("-", $time);
+                $time = $_time[2]."-".$_time[1]."-".$_time[0];
+                $time = strtotime($time);
+            }
+            return $btime + $time;
+        }else
+            $this->main->trigger_error("The time parameter can be either string or int");
+    }
+    function addSeconds($time, $count) {
+        return $time + $count;
+    }
+    function addMinutes($time, $count) {
+        return $time + (60*$count);
+    }
+    function addHours($time, $count) {
+        return $time + (60*60*$count);
+    }
+    function addDays($time, $count) {
+        return $time + (60*60*24*$count);
+    }
+    function addMonths($time, $count) {
+        return $time + (60*60*24*30*$count);
+    }
+    function addYears($time, $count) {
+        return $time + (60*60*24*365*$count);
+    }
 }
