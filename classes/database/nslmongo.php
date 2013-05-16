d18ed8439e52352b3be10541b8b2553f3c38d116
Danny Morabito <staff@newsocialife.com>
2013-05-16 18:05:26 +0200

    Allow subfolders plugins

--- classes/database/nslmongo.php
+++ classes/database/nslmongo.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 class nslmongo extends dbbase {
 	private $connection;
 	public $resource;
--- classes/db.php
+++ classes/db.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 include $GLOBALS["NSLWebsiteEngine/pluginspath"]."/database/base.php";
 include $GLOBALS["NSLWebsiteEngine/pluginspath"]."/database/nslmongo.php";
 class db extends nslmongo {
