1070f27467496ba8d2d1f6b9e7b85d6ecf2a9ab2
Danny Morabito <staff@newsocialife.com>
2013-07-05 00:07:28 +0200

    Fix for #34 #33 #32 #31

--- classes/templating.php
+++ classes/templating.php
@@ -2,22 +2,47 @@
 include_once __DIR__.DS."..".DS."vendor".DS."autoload.php";
 class templating extends base {
 	private $twig;
+    public $index;
+    public $views;
+    
 	function __construct(&$main) {
         parent::__construct($main);
-		$this->twig = new Twig_Environment(new Twig_Loader_Filesystem(realpath('views'.DS)), [
-			//'cache' => '/path/to/compilation_cache'
-		]);
+        if($this->configuration("index", "templating"))
+            $this->setIndex($this->configuration("index"), "templating");
+        else
+            $this->setIndex("index.twig");
+        if($this->configuration("views", "templating"))
+            $this->setViews($this->configuration("views", "templating"));
+        else
+            $this->setViews("views");
+        if($this->configuration("cache", "templating"))
+            $twigconfig = [
+            	'cache' => $this->configuration("cache", "templating")
+	    	];
+        else
+            $twigconfig = [];
+        $this->twig = new Twig_Environment(new Twig_Loader_Filesystem(realpath($this->views.DS), $twigconfig));
 	}
+    function setIndex($file) {
+        $this->index = $file;
+        return $this;
+    }
+    function setViews($views) {
+        $this->views = $views;
+        return $this;
+    }
 	function addFilter($name, $callback, $options = []) {
-		return $this->twig->addFilter(new Twig_SimpleFilter($name, $callback, $options));
+		$this->twig->addFilter(new Twig_SimpleFilter($name, $callback, $options));
+        return $this;
 	}
 	function addFunction($name, $callback, $options = []) {
-		return $this->twig->addFunction(new Twig_SimpleFunction($name, $callback, $options));
+		$this->twig->addFunction(new Twig_SimpleFunction($name, $callback, $options));
+        return $this;
 	}
 	function render($file, $options = []) {
 		if(!isset($options["filename"])) {
 			$options = array_merge($options, ["filename" => $file.".twig"]);
-			return $this->twig->render("index.twig", $options);
+			return $this->twig->render($this->index, $options);
 		}else{
 			$page = $options["filename"];
 			$options = array_merge($options, ["filename" => $file.".twig"]);
