3d0cdcbbbaf0948ddf7f5384e27ea8d2b7c7197a
Danny Morabito <staff@newsocialife.com>
2013-09-09 23:26:39 +0200

    Temporary fix for #59
    This works only if i18n is included before templating

--- classes/templating.php
+++ classes/templating.php
@@ -2,44 +2,53 @@
 include_once __DIR__.DS."..".DS."vendor".DS."autoload.php";
 class templating extends base {
 	private $twig;
-    public $index;
-    public $views;
-    public $__requirements_composer = array("twig/twig" => "*");
-    public $version = "1.0.1";
-    
+	public $index;
+	public $views;
+	public $__requirements_composer = array("twig/twig" => "*");
+	public $version = "1.0.1";
+	
 	function __construct(&$main) {
-        parent::__construct($main);
-        if($this->configuration("index", "templating"))
-            $this->setIndex($this->configuration("index"), "templating");
-        else
-            $this->setIndex("index.twig");
-        if($this->configuration("views", "templating"))
-            $this->setViews($this->configuration("views", "templating"));
-        else
-            $this->setViews("views");
-        if($this->configuration("cache", "templating"))
-            $twigconfig = array(
-            	'cache' => $this->configuration("cache", "templating")
-	    	);
-        else
-            $twigconfig = array();
-        $this->twig = new Twig_Environment(new Twig_Loader_Filesystem(realpath($this->views.DS), $twigconfig));
+		parent::__construct($main);
+		if($this->configuration("index", "templating"))
+			$this->setIndex($this->configuration("index"), "templating");
+		else
+			$this->setIndex("index.twig");
+		if($this->configuration("views", "templating"))
+			$this->setViews($this->configuration("views", "templating"));
+		else
+			$this->setViews("views");
+		if($this->configuration("cache", "templating"))
+			$twigconfig = array(
+				'cache' => $this->configuration("cache", "templating")
+			);
+		else
+			$twigconfig = array();
+		$this->twig = new Twig_Environment(new Twig_Loader_Filesystem(realpath($this->views.DS), $twigconfig));
+		$this->i18n = $this->main->is_included("i18n");
 	}
-    function setIndex($file) {
-        $this->index = $file;
-        return $this;
-    }
-    function setViews($views) {
-        $this->views = $views;
-        return $this;
-    }
+	function setLanguage($lang) {
+		$lib = $this->main;
+		$i18n = $this->i18n;
+		$lib->$i18n->set($lang);
+		$this->addFunction("i18n", function($text, $args = []) use($lib, $i18n) {
+			return $lib->$i18n->getTranslation($text, $args);
+		});
+	}
+	function setIndex($file) {
+		$this->index = $file;
+		return $this;
+	}
+	function setViews($views) {
+		$this->views = $views;
+		return $this;
+	}
 	function addFilter($name, $callback, $options = array()) {
 		$this->twig->addFilter(new Twig_SimpleFilter($name, $callback, $options));
-        return $this;
+		return $this;
 	}
 	function addFunction($name, $callback, $options = array()) {
 		$this->twig->addFunction(new Twig_SimpleFunction($name, $callback, $options));
-        return $this;
+		return $this;
 	}
 	function render($file, $options = array()) {
 		if(!isset($options["filename"])) {
